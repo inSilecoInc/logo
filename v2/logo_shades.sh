b7569ddf8ef2ad0ba7df8c2d595ce7512d7ff0f2
Kevin Cazelles <kevin.cazelles@insileco.io>
2025-01-17 13:48:06 -0500

    white version

--- v2/logo_shades.sh
+++ v2/logo_shades.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # declare -A cols
 # declare -A nams
-cols=( "#3FB3B2" "#AAD400" "#FFCC00" "#FF2A2A" )
-nams=( "turquoise" "yellowgreen" "gold" "red" ) 
+cols=( "#3FB3B2" "#AAD400" "#FFCC00" "#FF2A2A" "#FFFFFF" )
+nams=( "turquoise" "yellowgreen" "gold" "red" "white" ) 
 
 for f in "main" "cloud"
 do 
